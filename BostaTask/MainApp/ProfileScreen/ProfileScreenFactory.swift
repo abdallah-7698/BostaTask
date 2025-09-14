fe390f96dc8a98546d049792886ef44f329676b4
abdallah.mohammed <abdallah.mohamed@1pass.app>
2025-09-14 17:27:41 +0300

    - Organize the folder structure of the app - Remove the storyboards and make the app depends on programatic View Controller

---
+++ BostaTask/MainApp/ProfileScreen/ProfileScreenFactory.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileScreenFactory.swift
+//  BostaTask
+//
+//  Created by name on 14/09/2025.
+//
+
+import Foundation
+
+struct ProfileScreenFactory {
+  static func makeViewContrller() -> ProfileScreenViewController {
+    return ProfileScreenViewController()
+  }
+}
---
+++ BostaTask/MainApp/ProfileScreen/ProfileScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileScreenViewController.swift
+//  BostaTask
+//
+//  Created by name on 14/09/2025.
+//
+
+import UIKit
+
+class ProfileScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .red
+    }
+
+}
